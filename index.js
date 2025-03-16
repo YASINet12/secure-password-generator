9d05d833ead2a9799e017f85a96cdffd07412621
yassine <yassinetahery6@outlook.com>
2025-03-16 00:12:30 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "~&#|^@=+-/!?*$%¨¨£_";
+
+    let allowedChars = "";
+    let password = "";
+
+    if (includeLowercase) allowedChars += lowercaseChars;
+    if (includeUppercase) allowedChars += uppercaseChars;
+    if (includeNumbers) allowedChars += numberChars;
+    if (includeSymbols) allowedChars += symbolChars;
+
+    if (length <= 0) {
+        return "(Password length must be at least 1)";
+    }
+    if (allowedChars.length === 0) {
+        return "(At least one character type must be selected)";
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 1;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`);
